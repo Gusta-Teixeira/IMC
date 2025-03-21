8ab6e1c76ba129e47b8f7db0e03f2d3fed3af80b
gustavo <gustavotsalgado@gmail.com>
2025-03-20 21:45:42 -0300

    Primeiro commit

--- app/src/main/java/com/ifsc/imc/MainActivity.java
+++ app/src/main/java/com/ifsc/imc/MainActivity.java
@@ -1,7 +1,11 @@
 package com.ifsc.imc;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
@@ -11,12 +15,26 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btAbre;
+    EditText edText;
+    String wTexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+        btAbre = findViewById(R.id.button);
+        edText = findViewById(R.id.editTextText);
+        btAbre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), MsgActivity.class);
+                //wTexto = edText.getText().toString();
+                i.putExtra("msg", edText.getText().toString());
+                startActivity(i);
+            }
+        });
+
         Log.d("ciclodevida", "onCreate");
         Toast.makeText(this, "onCreate", Toast.LENGTH_LONG).show();
     }
---
+++ app/src/main/java/com/ifsc/imc/MsgActivity.java
@@ -0,0 +1,24 @@
+package com.ifsc.imc;
+
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MsgActivity extends AppCompatActivity {
+    TextView txTexto;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_msg);
+        txTexto = findViewById(R.id.tvMsg);
+
+        Bundle bundle = getIntent().getExtras();
+        txTexto.setText(bundle.getString("msg"));
+    }
+}
